61207213683182a2f29f8a98ce658e2b6ddd30b2
Pascal Hertleif <pascal@technocreatives.com>
2019-06-17 15:05:12 +0200

    Add X11 xkb files as fixtures
    Soooo much new stuff in there

--- tests/fixtures.rs
+++ tests/fixtures.rs
@@ -1,26 +1,46 @@
 use kbd_parser::parse;
-use std::{error::Error, ffi::OsStr, fs, path::Path};
+use std::{
+    error::Error,
+    ffi::OsStr,
+    fs,
+    path::{Path, PathBuf},
+};
 
 #[test]
 fn parse_fixtures() -> Result<(), Box<dyn Error>> {
     let xkb_files = fs::read_dir("tests/fixtures")?
         .filter_map(|x| x.ok())
-        .filter(|x| x.path().extension() == Some(OsStr::new("xkb")));
-    let mut failed = false;
+        .map(|x| x.path())
+        .filter(|x| x.is_file())
+        .filter(|x| x.extension() == Some(OsStr::new("xkb")));
+    parse_files(xkb_files)
+}
+
+#[test]
+fn parse_x11_fixtures() -> Result<(), Box<dyn Error>> {
+    let xkb_files = fs::read_dir("tests/fixtures/x11/symbols")?
+        .filter_map(|x| x.ok())
+        .map(|x| x.path())
+        .filter(|x| x.is_file());
+    parse_files(xkb_files)
+}
+
+fn parse_files(xkb_files: impl Iterator<Item = PathBuf>) -> Result<(), Box<dyn Error>> {
+    let mut failed = 0;
     for file in xkb_files {
-        if let Err(e) = parse_one_file(&file.path()) {
-            eprintln!("error parsing file {}:\n{}", file.path().display(), e);
-            failed = true;
+        if let Err(e) = parse_one_file(&file) {
+            panic!("error parsing file {}:\n{}", file.display(), e);
+            failed += 1;
         }
     }
-    if failed {
-        Err("failed")?
-    } else {
-        Ok(())
+    if failed > 0 {
+        Err(format!("{} tests failed", failed))?
     }
+    Ok(())
 }
 
 fn parse_one_file(file_name: &Path) -> Result<(), Box<dyn Error>> {
+    eprintln!("file: {}", file_name.display());
     let file = std::fs::read_to_string(&file_name)?;
     let _ = parse(&file)?;
     Ok(())
