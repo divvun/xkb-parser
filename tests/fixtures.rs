d97d4945c62a20f3e322ecab56171a5ab943ee14
Pascal Hertleif <pascal@technocreatives.com>
2019-06-17 20:51:50 +0200

    Improve test output

--- tests/fixtures.rs
+++ tests/fixtures.rs
@@ -34,25 +34,28 @@ fn parse_x11_fixtures() -> Result<(), Box<dyn Error>> {
 
 fn parse_files(xkb_files: impl Iterator<Item = PathBuf>) -> Result<(), Box<dyn Error>> {
     let mut failed = 0;
-    for file in xkb_files {
-        if let Err(e) = parse_one_file(&file) {
-            panic!("error parsing file {}:\n{}", file.display(), e);
+    for file_name in xkb_files.filter(|x| x.extension() != Some(OsStr::new("json"))) {
+        eprintln!("   next: {}", file_name.display());
+        if let Err(e) = parse_one_file(&file_name) {
+            if std::env::var("VERBOSE_PARSE_ERRORS") == Ok("1".to_owned()) {
+                eprintln!("error parsing file {}:\n{}", file_name.display(), e);
+            } else {
+                eprintln!(" failed: {}", file_name.display());
+            }
             failed += 1;
         }
     }
     if failed > 0 {
-        Err(format!("{} tests failed", failed))?
+        panic!("{} tests failed -- Set `VERBOSE_PARSE_ERRORS=1` for error messages", failed);
     }
     Ok(())
 }
 
 fn parse_one_file(file_name: &Path) -> Result<(), Box<dyn Error>> {
-    eprintln!("file: {}", file_name.display());
     let file = std::fs::read_to_string(&file_name)?;
     let mut parse_tree = XkbParser::parse(Rule::file, &file)?;
-    std::fs::write(file_name.with_extension(".debug.json"), parse_tree.to_json())?;
+    std::fs::write(file_name.with_extension("debug.json"), parse_tree.to_json())?;
     let _ =
         Xkb::from_pest(&mut parse_tree).map_err(|e| format!("ast generation failed: {:?}", e))?;
-
     Ok(())
 }
