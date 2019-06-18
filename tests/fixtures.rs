80d423c97ca6de2c9279a86ee1884c693b14b522
Pascal Hertleif <pascal@technocreatives.com>
2019-06-18 10:53:14 +0200

    Make tests run way faster

--- tests/fixtures.rs
+++ tests/fixtures.rs
@@ -1,15 +1,18 @@
 use from_pest::FromPest;
 use kbd_parser::{Rule, Xkb, XkbParser};
 use pest::Parser;
+use rayon::prelude::*;
 use std::{
-    error::Error,
+    error::Error as StdError,
     ffi::OsStr,
     fs,
     path::{Path, PathBuf},
 };
 
+type Error = Box<dyn StdError + Send + Sync>;
+
 #[test]
-fn parse_fixtures() -> Result<(), Box<dyn Error>> {
+fn parse_fixtures() -> Result<(), Error> {
     let _ = env_logger::builder().is_test(true).try_init();
 
     let xkb_files = fs::read_dir("tests/fixtures")?
@@ -22,7 +25,7 @@ fn parse_fixtures() -> Result<(), Box<dyn Error>> {
 
 #[test]
 #[ignore]
-fn parse_x11_fixtures() -> Result<(), Box<dyn Error>> {
+fn parse_x11_fixtures() -> Result<(), Error> {
     let _ = env_logger::builder().is_test(true).try_init();
 
     let xkb_files = fs::read_dir("tests/fixtures/x11/symbols")?
@@ -32,26 +35,31 @@ fn parse_x11_fixtures() -> Result<(), Box<dyn Error>> {
     parse_files(xkb_files)
 }
 
-fn parse_files(xkb_files: impl Iterator<Item = PathBuf>) -> Result<(), Box<dyn Error>> {
-    let mut failed = 0;
-    for file_name in xkb_files.filter(|x| x.extension() != Some(OsStr::new("json"))) {
-        eprintln!("   next: {}", file_name.display());
-        if let Err(e) = parse_one_file(&file_name) {
+fn parse_files(xkb_files: impl Iterator<Item = PathBuf> + Send) -> Result<(), Error> {
+    let failed: usize = xkb_files
+        .filter(|f| f.extension() != Some(OsStr::new("json")))
+        .par_bridge()
+        .map(|f: PathBuf| {
+            let res = parse_one_file(&f);
+            (f, res)
+        })
+        .filter_map(|(f, x)| x.err().map(|e| (f, e)))
+        .inspect(|(file_name, error)| {
             if std::env::var("VERBOSE_PARSE_ERRORS") == Ok("1".to_owned()) {
-                eprintln!("error parsing file {}:\n{}", file_name.display(), e);
+                eprintln!("error parsing file {}:\n{}", file_name.display(), error);
             } else {
                 eprintln!(" failed: {}", file_name.display());
             }
-            failed += 1;
-        }
-    }
+        })
+        .count();
+
     if failed > 0 {
         panic!("{} tests failed -- Set `VERBOSE_PARSE_ERRORS=1` for error messages", failed);
     }
     Ok(())
 }
 
-fn parse_one_file(file_name: &Path) -> Result<(), Box<dyn Error>> {
+fn parse_one_file(file_name: &Path) -> Result<(), Error> {
     let file = std::fs::read_to_string(&file_name)?;
     let mut parse_tree = XkbParser::parse(Rule::file, &file)?;
     std::fs::write(file_name.with_extension("debug.json"), parse_tree.to_json())?;
