4320dfbda3fa42f2baa96cc05292f9675ab93b96
Pascal Hertleif <pascal@technocreatives.com>
2019-06-18 13:23:29 +0200

    Slightly nicer debug output
    [ron] is kinda like default Rust debug output, setting this file type
    will give you some kind of usable syntax highlighting at least.
    
    [ron]: https://github.com/ron-rs/ron

--- tests/fixtures.rs
+++ tests/fixtures.rs
@@ -95,7 +95,7 @@ fn parse_files(xkb_files: impl ParallelIterator<Item = PathBuf>) -> Result<(), E
 fn parse_one_file(file_name: &Path) -> Result<(), Error> {
     let file = std::fs::read_to_string(&file_name)?;
     let mut parse_tree = XkbParser::parse(Rule::file, &file)?;
-    std::fs::write(file_name.with_extension("debug.json"), parse_tree.to_json())?;
+    std::fs::write(file_name.with_extension("debug.ron"), format!("{:#?}", parse_tree))?;
     let _ =
         Xkb::from_pest(&mut parse_tree).map_err(|e| format!("ast generation failed: {:?}", e))?;
     Ok(())
