97b818aa38782f0afdd9cc4a29ba8abe7254f591
Pascal Hertleif <pascal@technocreatives.com>
2019-07-18 18:22:23 +0200

    Add keysym to unicode

--- x11-keysymdef/build.rs
+++ x11-keysymdef/build.rs
@@ -7,7 +7,55 @@ use std::{
 };
 
 fn main() {
-    let path = Path::new(&env::var("OUT_DIR").unwrap()).join("codegen.rs");
+    from_header();
+    from_tsv();
+}
+
+fn from_tsv() {
+    let path = Path::new(&env::var("OUT_DIR").unwrap()).join("from_tsv.rs");
+    let mut file = BufWriter::new(File::create(&path).unwrap());
+
+    let tsv = read_to_string("src/keysym.tsv").unwrap();
+    let mapping: Vec<_> = tsv
+        .lines()
+        .skip(47)
+        .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
+        .map(|x| (x[0], x[1]))
+        .collect();
+
+    writeln!(&mut file, "lazy_static::lazy_static! {{").unwrap();
+
+    writeln!(
+        &mut file,
+        "static ref NAME_TO_CODEPOINT: std::collections::HashMap<&'static str, char> = {{"
+    )
+    .unwrap();
+    writeln!(&mut file, "let mut map = std::collections::HashMap::new();").unwrap();
+    for (name, code) in &mapping {
+        writeln!(&mut file, r#"map.entry("{}").or_insert('\u{{{}}}');"#, name, code).unwrap();
+    }
+    writeln!(&mut file, "map").unwrap();
+    writeln!(&mut file, "}};").unwrap();
+
+    writeln!(
+        &mut file,
+        "static ref CODEPOINT_TO_NAME: std::collections::HashMap<char, &'static str> = {{"
+    )
+    .unwrap();
+    writeln!(&mut file, "let mut map = std::collections::HashMap::new();").unwrap();
+
+    for (name, code) in &mapping {
+        writeln!(&mut file, r#"map.entry('\u{{{}}}').or_insert("{}");"#, code, name).unwrap();
+    }
+
+    writeln!(&mut file, "map").unwrap();
+    writeln!(&mut file, "}};").unwrap();
+
+    writeln!(&mut file, "}}").unwrap();
+}
+
+fn from_header() {
+    let path = Path::new(&env::var("OUT_DIR").unwrap()).join("from_header.rs");
     let mut file = BufWriter::new(File::create(&path).unwrap());
     let header_file = read_to_string("src/keysymdef.h").unwrap();
     let definitions = read_defs(&header_file).unwrap();
@@ -33,7 +81,6 @@ fn main() {
     .unwrap();
     writeln!(&mut file, "let mut map = std::collections::HashMap::new();").unwrap();
     for Define { name, code } in &definitions {
-        writeln!(&mut file, r#"map.insert({}, "{}");"#, code, name).unwrap();
         writeln!(&mut file, r#"map.entry({}).or_insert("{}");"#, code, name).unwrap();
     }
     writeln!(&mut file, "map").unwrap();
--- x11-keysymdef/src/lib.rs
+++ x11-keysymdef/src/lib.rs
@@ -8,27 +8,55 @@
 
 #![allow(clippy::unreadable_literal)]
 
-include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
+include!(concat!(env!("OUT_DIR"), "/from_header.rs"));
+include!(concat!(env!("OUT_DIR"), "/from_tsv.rs"));
 
 /// Look up a keysym code by the mnemonic macro name
-pub fn string_to_keysym(name: &str) -> Option<u32> {
+pub fn name_to_keysym(name: &str) -> Option<u32> {
     NAME_TO_KEYSYM.get(&name).copied()
+}
+
+/// Look up a unicode char (unicode code point) by the mnemonic macro name
+pub fn name_to_codepoint(name: &str) -> Option<char> {
+    NAME_TO_CODEPOINT.get(&name).copied()
 }
 
 /// Look up a mnemonic macro name by the keysym code
 ///
 /// Note that one code can have multiple names. The first one in the definition
 /// file will be returned.
-pub fn keysym_to_string(keysym: u32) -> Option<&'static str> {
+pub fn keysym_to_name(keysym: u32) -> Option<&'static str> {
     KEYSYM_TO_NAME.get(&keysym).copied()
+}
+
+/// Look up a mnemonic macro name by the keysym code
+///
+/// Note that one code can have multiple names. The first one in the definition
+/// file will be returned.
+pub fn char_to_name(codepoint: char) -> Option<&'static str> {
+    CODEPOINT_TO_NAME.get(&codepoint).copied()
+}
+
+/// Look up unicode codepoint for keysym
+pub fn keysym_to_char(keysym: u32) -> Option<char> {
+    keysym_to_name(keysym).and_then(|name| name_to_codepoint(name))
+}
+
+/// Look up keysym for unicode codepoint
+pub fn char_to_keysym(codepoint: char) -> Option<u32> {
+    char_to_name(codepoint).and_then(|name| name_to_keysym(name))
 }
 
 #[test]
 fn what_code_is_this() {
-    assert!(string_to_keysym("Uhorngrave").is_some());
+    assert!(name_to_keysym("Uhorngrave").is_some());
+    assert!(name_to_codepoint("Uhorngrave").is_some());
+    assert!(char_to_keysym('\u{1EEA}').is_some());
 }
 
 #[test]
 fn how_do_you_call_this() {
-    assert!(keysym_to_string(0x10004b3).is_some());
+    assert!(keysym_to_name(0x10004b3).is_some());
+    assert!(keysym_to_char(0x10004b3).is_some());
+    assert!(char_to_name('\u{1EEA}').is_some());
 }
